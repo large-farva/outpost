c21f319f2bcd69cb35f0649f9e59c013f468e919
large-farva <sebastianvencill@gmail.com>
2025-09-07 08:29:37 +0300

    update: Firefox policies and base package removal.

--- files/scripts/remove-packages.sh
+++ files/scripts/remove-packages.sh
@@ -5,7 +5,6 @@ rpm-ostree override remove \
   gnome-extensions-app \
   vim \
   tmux \
-  gnome-shell-extension-appindicator \
   gnome-shell-extension-background-logo \
   gnome-backgrounds \
   || true
