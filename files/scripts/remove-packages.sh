be2e3496e076fe6c6d5d6c793f110ffd6ee485e6
large-farva <sebastianvencill@gmail.com>
2025-11-14 11:54:06 +0300

    fix: remove-packages.sh

--- files/scripts/remove-packages.sh
+++ files/scripts/remove-packages.sh
@@ -4,28 +4,31 @@ set -ouex pipefail
 dnf5 remove \
   fedora-bookmarks \
   fedora-chromium-config* \
-  gdouros-symbola-fonts \
   gnome-shell-extension-background-logo \
   gnome-shell-extension-launch-new-instance \
   gnome-shell-extension-window-list \
-  google-noto-naskh-arabic-vf-fonts \
-  google-noto-sans* \
-  google-noto-serif* \
   htop \
-  jomolhari-fonts \
-  julietaula-montserrat-fonts \
-  madan-fonts \
   nvtop \
-  paktype-naskh-basic-fonts \
-  rit-meera-new-fonts \
-  rit-rachana-fonts \
-  sil-padauk-fonts \
   tmux \
-  vazirmatn-vf-fonts \
   vim* \
   || true
 
 # Something is causing the build to delete 400 packages at build time. WTF
+
 # gnome-tour \
 # yelp* \
 # default-fonts* \
+# madan-fonts \
+# gdouros-symbola-fonts \
+# vazirmatn-vf-fonts \
+# google-noto-naskh-arabic-vf-fonts \
+# google-noto-sans* \
+# google-noto-serif* \
+# jomolhari-fonts \
+# julietaula-montserrat-fonts \
+# paktype-naskh-basic-fonts \
+# rit-meera-new-fonts \
+# rit-rachana-fonts \
+# sil-padauk-fonts \
+
+
