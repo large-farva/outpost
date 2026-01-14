f39b8dedc53a72e7c26a08555dc5eef223d8c519
large-farva <sebastianvencill@gmail.com>
2026-01-14 10:54:51 -0800

    update: theming to splash, plymouth, and color-scheme

--- files/scripts/cleanup.sh
+++ files/scripts/cleanup.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+# Temperary solution for cleaning up branding form base image.
+
+# Remove Aurora and Fedora backgrounds
 rm -rf /usr/share/backgrounds/aurora/
 rm -rf /usr/share/backgrounds/f43/
 rm -rf /usr/share/backgrounds/fedora-workstation/
 rm -rf /usr/share/backgrounds/images/
 
+# Remove Aurora's other watermark in Spinner plymouth theme.
+rm /usr/share/plymouth/themes/spinner/kinoite-watermark.png
+
+# Remove Aurora's icons
 rm /usr/share/icons/hicolor/scalable/places/auroalogo*
 
+# Remove Aurora's autostart entries that don't apply to Outpost
 rm /etc/xdg/autostart/orca-autostart.desktop
 rm /etc/xdg/autostart/vboxclient.desktop
 rm /etc/xdg/autostart/vmware-user.desktop
--- files/scripts/plymouth.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-THEME="outpost"
-
-/usr/bin/plymouth-set-default-theme "$THEME"
