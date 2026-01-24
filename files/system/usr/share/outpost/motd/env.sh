f417f9e62d6f81d4d24305018c20461b4e60e87b
large-farva <sebastianvencill@gmail.com>
2026-01-23 18:46:39 -0800

    feat: terminal tweaks, custom dod-trust module add: Bazaar remove: Discover

--- files/system/usr/share/outpost/motd/env.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-export MOTD_IMAGE_NAME="$(jq -rc '."image-ref"' "${MOTD_IMAGE_INFO_FILE:-/usr/share/outpost/image-info.json}" | sed 's@ostree-image-signed:docker://@@')"
-
-export MOTD_IMAGE_TAG="$(jq -rc '."image-tag"' "${MOTD_IMAGE_INFO_FILE:-/usr/share/outpost/image-info.json}")"
